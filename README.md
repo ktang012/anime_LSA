bc051ca4bbae42f0301a371d5fd6440d2158d9ad
Kevin Tang <ktang012@ucr.edu>
2017-12-25 16:37:27 -0800

    update data files

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 - Predict how well a currently airing anime will be received by reddit
 - Data visualization of anime discussion
 - What makes an anime good? (to reddit)
+
+
+### Todo + reminders
+- Playing with toy data set using spring 2017 animes, will get comments from other seasons later
+- Need to test classifiers and implement word pruning
+- Cluster data and identify clusters and corresponding comments
--- src/scripts/preprocess.py
+++ src/scripts/preprocess.py
@@ -1,6 +1,9 @@
+# Preprocess the data via tokenizing comments, removing some stopwords, punctuation, etc
+# Place into
+
 import os
-import re
 import json
+import re
 
 from nltk.tokenize import TweetTokenizer
 from nltk.stem.porter import *
@@ -12,95 +15,144 @@ anime_season = "spring_2017"
 data_path = os.path.join("..", "..", "data", anime_season)
 
 stop_words = [
-    "a", "above", "all", "am", "an", "and", "any", "are", "as", "at",
+    "a", "above", "all", "also", "am", "an", "and", "any", "are", "as", "at",
     "be", "been", "but", "by",
     "can", "could",
     "did", "do", "does",
     "each",
-    "few", "for", "from",
+    "few", "for", "from", "fuck",
+    "get",
     "had", "has", "have", "he", "her", "here", "him","himself", "his", "how",
     "i", "if", "in", "is", "it", "its", "itself",
     "just",
-    "let",
+    "let", "like", "lol",
     "me", "my", "myself",
-    "no", "nor",
-    "of", "off", "on", "once", "only", "or", "our", "ourselves", "out",
+    "no", "nor", "now",
+    "of", "off", "on", "once", "one", "only", "or", "our", "ourselves", "out",
     "she", "so", "such",
-    "than", "that", "the", "their", "them", "then", "they", "this", "those", "to", "too",
+    "than", "that", "the", "there", "their", "them", "then", "these", "they", "this", "those", "to", "too", "two",
     "under", "up",
     "very",
-    "was", "we", "what", "when", "where", "who", "why", "with", "would",
+    "was", "we", "were", "what", "when", "where", "who", "why", "will", "with", "would",
     "you", "your", "yourself"
 ]
 
 link_pattern = "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
 non_alpha_pattern = "[^a-zA-Z]+"
 
+
+label_counter = 0
+
+preprocessed_path = os.path.join("..", "..", "data", "preprocessed", anime_season)
+if not os.path.exists(preprocessed_path):
+    os.mkdir(preprocessed_path)
+
+season_dict = {
+    "season": 0,
+    "year": 2017,
+    "animes": []
+}
+season_animes = []
+
+anime_id_mapping = {}
+
 for path in os.walk(data_path):
     anime_path = path[0]
     episode_paths = path[2]
-    for ep in episode_paths:
-        ep_path = os.path.join(anime_path, ep)
-        ep_thread = json.load(open(ep_path))
-        ep_links = []
-        ep_comments = []
-        ep_comments_meta = []
-        for comment in ep_thread["comments"]:
-            ep_comment_dict = {
-                "score": comment["score"],
-                "id": comment["id"],
-                "pid": comment["pid"],
-                "date": comment["date"],
-                "depth": comment["depth"],
-                "text": []
-            }
 
+    if not episode_paths:
+        continue
+    else:
+        anime_name = os.path.split(anime_path)[-1]
 
-            tokenized_comment = tweet_tok.tokenize(comment["text"])
-            preprocessed_comment = []
-            for tok in tokenized_comment:
-                if re.match(link_pattern, tok):
-                    ep_links.append(tok)
-                else:
+        anime_dict = {
+            "anime_name": anime_name,
+            "anime_id": label_counter,
+            "ep_submissions": []
+        }
+
+        anime_id_mapping[label_counter] = anime_name
+
+        label_counter += 1
+
+        ep_submissions = []
+        for ep in episode_paths:
+            ep_path = os.path.join(anime_path, ep)
+            ep_thread = json.load(open(ep_path))
+            ep_links = []
+            ep_comments = []
+            ep_comments_meta = []
+            ep_num = re.findall("\d+", ep)
+
+            for comment in ep_thread["comments"]:
+
+                ep_comment_dict = {
+                    "score": comment["score"],
+                    "id": comment["id"],
+                    "pid": comment["pid"],
+                    "date": comment["date"],
+                    "depth": comment["depth"],
+                    "text": []
+                }
+
+                tokenized_comment = tweet_tok.tokenize(comment["text"])
+                preprocessed_comment = []
+                for tok in tokenized_comment:
+                    if re.match(link_pattern, tok):
+                        ep_links.append(tok)
+                    else:
+                        if len(tok) <= 2:
+                            continue
+                        elif tok in stop_words:
+                            continue
+                        else:
+                            preprocessed_comment.append(tok.lower())
+
+                preprocessed_comment = [port_stem.stem(tok) for tok in preprocessed_comment]
+                preprocessed_comment = [re.sub(non_alpha_pattern, "", tok) for tok in preprocessed_comment]
+
+                clean_comment = []
+                for tok in preprocessed_comment:
                     if len(tok) <= 2:
                         continue
                     elif tok in stop_words:
                         continue
                     else:
-                        preprocessed_comment.append(tok.lower())
+                        clean_comment.append(tok)
 
-            preprocessed_comment = [port_stem.stem(tok) for tok in preprocessed_comment]
-            preprocessed_comment = [re.sub(non_alpha_pattern, "", tok) for tok in preprocessed_comment]
+                ep_comment_dict["text"] = clean_comment
+                ep_comments_meta.append(ep_comment_dict)
 
-            clean_comment = []
-            for tok in preprocessed_comment:
-                if len(tok) <= 2:
-                    continue
-                elif tok in stop_words:
-                    continue
-                else:
-                    clean_comment.append(tok)
+            if not os.path.exists(preprocessed_path):
+                os.makedirs(preprocessed_path)
 
-            ep_comment_dict["text"] = clean_comment
-            ep_comments_meta.append(ep_comment_dict)
+            submission_dict = {
+                "score": ep_thread["score"],
+                "episode_num": ep_num,
+                "id": ep_thread["id"],
+                "num_comments": ep_thread["num_comments"],
+                "date": ep_thread["date"],
+                "comments": ep_comments_meta
+            }
 
-        anime_name = anime_path.split("/")[4]
-        preprocessed_path = os.path.join("..", "..", "data", "preprocessed",
-                                         anime_season, anime_name)
+            ep_submissions.append(submission_dict)
 
-        if not os.path.exists(preprocessed_path):
-            os.makedirs(preprocessed_path)
+        anime_dict["ep_submissions"] = ep_submissions
 
-        submission_dict = {
-            "score": ep_thread["score"],
-            "id": ep_thread["id"],
-            "num_comments": ep_thread["num_comments"],
-            "date": ep_thread["date"],
-            "comments": ep_comments_meta
-        }
+        preprocessed_anime_file = os.path.join(preprocessed_path, anime_name + ".json")
 
-        submission_fname = os.path.join(preprocessed_path, ep)
+        with open(preprocessed_anime_file, "w") as file:
+            json.dump(anime_dict, file, indent=1, default=str)
 
-        with open(submission_fname, 'w') as file:
-            json.dump(submission_dict, file, indent=1, default=str)
+        season_animes.append(anime_dict)
+
+season_dict["animes"] = season_animes
+
+packaged_season_file = os.path.join(preprocessed_path, anime_season + ".json")
+with open(packaged_season_file, "w") as file:
+    json.dump(season_dict, file, indent=1, default=str)
+
+anime_id_mapping_file = os.path.join(preprocessed_path, "anime_mapping.json")
+with open(anime_id_mapping_file, "w") as file:
+    json.dump(anime_id_mapping, file, indent=1, default=str)
 
